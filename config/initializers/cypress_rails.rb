f0182edaa97ceedce30d2ef1ca2f52d2ec498edb
Jackie <jackie15910@gmail.com>
2023-08-31 01:05:21 -0400

    Added home page cypress

--- config/initializers/cypress_rails.rb
+++ config/initializers/cypress_rails.rb
@@ -4,7 +4,23 @@ DatabaseCleaner.strategy = :truncation
 return unless Rails.env.test?
 
 CypressRails.hooks.before_server_start do
-  # Called once, before either the transaction or the server is started
+  cat1 = Category.find_or_create_by!(name: 'Evergreens')
+
+  cat1.products.create!(
+    name: 'Giant Tea',
+    description: 'The Giant Tea is an uncommon...',
+    image: open_asset('plante_1.jpg'),
+    quantity: 0,
+    price: 64.99
+  )
+
+  cat1.products.create!(
+    name: 'Scented Blade',
+    description: 'The Scented Blade is an extremely rare...',
+    image: open_asset('plante_2.jpg'),
+    quantity: 18,
+    price: 24.99
+  )
 end
 
 CypressRails.hooks.after_transaction_start do
@@ -17,4 +33,10 @@ end
 
 CypressRails.hooks.before_server_stop do
   # Called once, at_exit
+end
+
+private
+
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
